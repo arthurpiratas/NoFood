9162abadac8ae89c07b3dd0784cdf1f34f909b0d
arthurpiratas <silva.arthursoares@gmail.com>
2021-02-19 00:08:21 -0300

    Atualização da classe repositorio usuario, iniciando a autenticação de usuario

--- NoFood.Api/repositories/usuario-repository.js
+++ NoFood.Api/repositories/usuario-repository.js
@@ -1,31 +1,43 @@
 require('../models/usuario-model')
 const base = require('../bin/base/repository-base')
+const md5 = require('md5')
 
 
 class usuarioRepository{
 
     constructor(){
         this._base = new base('UsuarioModel')
+        this._projection = 'nome email _id'
     }
 
-    async autentication(email, senha){
-        this._base
+    async autentication(Email, Senha){
+        let _hashSenha = md5(Senha) // criptografando a senha recebida 
+        this._base._model.findOne({email: Email, senha: _hashSenha}, this._projection) /* busco o usuario a partir do email e senha criptografada
+        e retorno a informação do nome, email e ID*/
     }
 
     async create(data){
-        return await this._base.create(data)
+        let usuarioCriado = await this._base.create(data)
+        return this._base._model.findById(usuarioCriado._id, this._projection)
     }
 
     async update(id, data){
-        return await this._base.update(id,data)
+        let newData = {
+            nome: data.nome, 
+            email: data.email, 
+            foto: data.foto
+        }
+        let usuarioAtualizado = await this._base.update(id,newData)
+
+        return this._base._model.findById(usuarioAtualizado._id, this._projection)
     }
 
     async getAll(){
-        return await this._base.getAll()
+        return await this._base._model.find({}, this._projection)
     }
 
     async getById(id){
-        return await this._base.getById(id)
+        return await this._base._model.findById(id, 'nome email foto')
     }
 
     async delete(id){
